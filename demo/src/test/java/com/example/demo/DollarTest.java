ec028a27926d818a5e40e2537092e6bbd2556cab
JinuCheon <woojin8787@gmail.com>
2024-06-16 12:47:52 +0900

    rename dollor -> dollar

--- demo/src/test/java/com/example/demo/DollarTest.java
+++ demo/src/test/java/com/example/demo/DollarTest.java
@@ -2,14 +2,14 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 
-public class DollorTest {
+public class DollarTest {
 
     @Test
     void testMultiplication() {
-        final Dollor five = new Dollor(5);
+        final Dollar five = new Dollar(5);
         five.times(2);
         assertEqual(10, five.amout);
     }
 
-    private class Dollor {}
+    private class Dollar {}
 }
