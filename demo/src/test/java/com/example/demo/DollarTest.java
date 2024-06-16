5214989c4c17d10db1c566ad356795b1bb1ffe2a
JinuCheon <woojin8787@gmail.com>
2024-06-16 12:52:30 +0900

    add assertj lib

--- demo/src/test/java/com/example/demo/DollarTest.java
+++ demo/src/test/java/com/example/demo/DollarTest.java
@@ -2,13 +2,15 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class DollarTest {
 
     @Test
     void testMultiplication() {
         final Dollar five = new Dollar(5);
         five.times(2);
-        assertEqual(10, five.amout);
+        assertThat(five.amout).isEqualTo(10);
     }
 
     private class Dollar {
