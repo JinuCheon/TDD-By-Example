f30898ddfce08412ace332095a8c23f7bec99a4d
JinuCheon <woojin8787@gmail.com>
2024-07-02 17:44:44 +0900

    Chapter 2 (#3)
    * add test: compile fail
    
    * make it compilable. but still fail
    
    * make test success

--- demo/src/test/java/com/example/demo/DollarTest.java
+++ demo/src/test/java/com/example/demo/DollarTest.java
@@ -9,8 +9,10 @@ public class DollarTest {
     @Test
     void testMultiplication() {
         final Dollar five = new Dollar(5);
-        five.times(2);
-        assertThat(five.amount).isEqualTo(10);
+        Dollar product = five.times(2);
+        assertThat(product.amount).isEqualTo(10);
+        product = five.times(3);
+        assertThat(product.amount).isEqualTo(15);
     }
 
     private class Dollar {
@@ -20,8 +22,8 @@ public class DollarTest {
             this.amount = amount;
         }
 
-        public void times(final int multipler) {
-            amount *= multipler;
+        public Dollar times(final int multipler) {
+            return new Dollar(amount * multipler);
         }
     }
 }
