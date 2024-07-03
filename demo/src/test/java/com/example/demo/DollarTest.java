48bd3e6e7d7e1d6536a9c04a43c14aa230d02ab6
JinuCheon <jinwoo.cheon@ktown4u.com>
2024-07-03 12:55:24 +0900

    값 객체 자체를 비교하는 것으로 변경!

--- demo/src/test/java/com/example/demo/DollarTest.java
+++ demo/src/test/java/com/example/demo/DollarTest.java
@@ -9,10 +9,8 @@ public class DollarTest {
     @Test
     void testMultiplication() {
         final Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertThat(product.amount).isEqualTo(10);
-        product = five.times(3);
-        assertThat(product.amount).isEqualTo(15);
+        assertThat(five.times(2)).isEqualTo(new Dollar(10));
+        assertThat(five.times(2)).isEqualTo(new Dollar(10));
     }
 
     @Test
