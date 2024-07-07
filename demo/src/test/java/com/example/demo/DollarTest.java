7b4e053d83eb6f4eb05f560974a315de2688b434
JinuCheon <woojin8787@gmail.com>
2024-07-07 19:40:36 +0900

    상속 구현

--- demo/src/test/java/com/example/demo/DollarTest.java
+++ demo/src/test/java/com/example/demo/DollarTest.java
@@ -19,8 +19,7 @@ public class DollarTest {
         assertThat(new Dollar(5).equals(new Dollar(6))).isFalse();
     }
 
-    private class Dollar {
-        private final int amount;
+    private class Dollar extends Money {
 
         public Dollar(final int amount) {
             this.amount = amount;
