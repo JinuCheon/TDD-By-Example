9ac57226a22467b770473da28d023313dcb75ca1
JinuCheon <woojin8787@gmail.com>
2024-07-11 01:55:05 +0900

    Chapter 8 (#9)
    * money 의 생성자를 factory 로 변경
    
    * times 를 추상클래스로 만들어서, 테스트 성공하게 변경
    
    * 다른 코드들도 factory 로 대체
    
    * clean up codes
    
    * franc 도 factory 로 변경
    
    * clean up code
    
    * add docs

--- demo/src/test/java/com/example/demo/DollarTest.java
+++ demo/src/test/java/com/example/demo/DollarTest.java
@@ -4,61 +4,73 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class DollarTest {
+class DollarTest {
 
     @Test
     void testMultiplication() {
-        final Dollar fiveDollar = new Dollar(5);
-        assertThat(fiveDollar.times(2)).isEqualTo(new Dollar(10));
-        assertThat(fiveDollar.times(2)).isEqualTo(new Dollar(10));
+        final Money fiveDollar = Money.dollar(5);
+        assertThat(fiveDollar.times(2)).isEqualTo(Money.dollar(10));
+        assertThat(fiveDollar.times(2)).isEqualTo(Money.dollar(10));
 
         final Franc fiveFranc = new Franc(5);
-        assertThat(fiveFranc.times(2)).isEqualTo(new Franc(10));
-        assertThat(fiveFranc.times(2)).isEqualTo(new Franc(10));
+        assertThat(fiveFranc.times(2)).isEqualTo(Money.franc(10));
+        assertThat(fiveFranc.times(2)).isEqualTo(Money.franc(10));
     }
 
     @Test
     void testEquality() {
-        assertThat(new Dollar(5).equals(new Dollar(5))).isTrue();
-        assertThat(new Dollar(5).equals(new Dollar(6))).isFalse();
-        assertThat(new Franc(5).equals(new Franc(5))).isTrue();
-        assertThat(new Franc(5).equals(new Franc(6))).isFalse();
-        assertThat(new Franc(5).equals(new Dollar(5))).isFalse();
+        assertThat(Money.dollar(5).equals(Money.dollar(5))).isTrue();
+        assertThat(Money.dollar(5).equals(Money.dollar(6))).isFalse();
+        assertThat(Money.franc(5).equals(Money.franc(5))).isTrue();
+        assertThat(Money.franc(5).equals(Money.franc(6))).isFalse();
+        assertThat(Money.franc(5).equals(Money.dollar(5))).isFalse();
     }
 
-    private class Dollar extends Money {
+    private abstract static class Money {
 
-        public Dollar(final int amount) {
-            this.amount = amount;
+        int amount;
+        static Money dollar(final int amount) {
+            return new Dollar(amount);
         }
 
-        public Dollar times(final int multipler) {
-            return new Dollar(amount * multipler);
-        }
-    }
-
-    private class Franc extends Money {
-
-        public Franc(final int amount) {
-            this.amount = amount;
+        static Money franc(final int amount) {
+            return new Franc(amount);
         }
 
-        public Franc times(final int multipler) {
-            return new Franc(amount * multipler);
-        }
-
-        public boolean equals(final Franc dollar) {
-            return dollar.amount == amount;
-        }
-    }
-
-    private class Money {
-        protected int amount;
-
-        public boolean equals(final Object object) {
-            final Money dollar = (Money) object;
+        public boolean equals(final Object obj) {
+            final Money dollar = (Money) obj;
             return amount == dollar.amount
                     && getClass().equals(dollar.getClass());
+        }
+
+        public abstract Money times(final int multiplier);
+
+    }
+
+    private static final class Dollar extends Money {
+
+        private Dollar(final int amount) {
+            this.amount = amount;
+        }
+
+        public Money times(final int multiplier) {
+            return new Dollar(amount * multiplier);
+        }
+    }
+
+    private static final class Franc extends Money {
+
+
+        private Franc(final int amount) {
+            this.amount = amount;
+        }
+
+        public Money times(final int multiplier) {
+            return new Franc(amount * multiplier);
+        }
+
+        boolean equals(final Franc dollar) {
+            return dollar.amount == amount;
         }
     }
 
