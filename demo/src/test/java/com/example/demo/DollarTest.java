4ab55a6744d41b87f0c5c0bd1382307078947040
JinuCheon <jinwoo.cheon@ktown4u.com>
2024-07-10 13:04:02 +0900

    예제코드 정리

--- demo/src/test/java/com/example/demo/DollarTest.java
+++ demo/src/test/java/com/example/demo/DollarTest.java
@@ -8,15 +8,21 @@ public class DollarTest {
 
     @Test
     void testMultiplication() {
-        final Dollar five = new Dollar(5);
-        assertThat(five.times(2).equals(new Dollar(10))).isTrue();
-        assertThat(five.times(2).equals(new Dollar(10))).isTrue();
+        final Dollar fiveDollar = new Dollar(5);
+        assertThat(fiveDollar.times(2)).isEqualTo(new Dollar(10));
+        assertThat(fiveDollar.times(2)).isEqualTo(new Dollar(10));
+
+        final Franc fiveFranc = new Franc(5);
+        assertThat(fiveFranc.times(2)).isEqualTo(new Franc(10));
+        assertThat(fiveFranc.times(2)).isEqualTo(new Franc(10));
     }
 
     @Test
-    public void testEquality() {
+    void testEquality() {
         assertThat(new Dollar(5).equals(new Dollar(5))).isTrue();
         assertThat(new Dollar(5).equals(new Dollar(6))).isFalse();
+        assertThat(new Franc(5).equals(new Franc(5))).isTrue();
+        assertThat(new Franc(5).equals(new Franc(6))).isFalse();
     }
 
     private class Dollar extends Money {
@@ -29,4 +35,29 @@ public class DollarTest {
             return new Dollar(amount * multipler);
         }
     }
+
+    private class Franc extends Money {
+
+        public Franc(final int amount) {
+            this.amount = amount;
+        }
+
+        public Franc times(final int multipler) {
+            return new Franc(amount * multipler);
+        }
+
+        public boolean equals(final Franc dollar) {
+            return dollar.amount == amount;
+        }
+    }
+
+    private class Money {
+        protected int amount;
+
+        public boolean equals(final Object object) {
+            final Money dollar = (Money) object;
+            return amount == dollar.amount;
+        }
+    }
+
 }
