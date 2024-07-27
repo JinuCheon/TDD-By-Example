ce7add5d80054956a87442255ba2ff8a44ce19c5
JinuCheon <woojin8787@gmail.com>
2024-07-27 11:57:42 +0900

    14

--- demo/src/test/java/com/example/demo/DollarTest.java
+++ demo/src/test/java/com/example/demo/DollarTest.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DollarTest {
@@ -63,8 +66,26 @@ class DollarTest {
         assertThat(Money.dollar(1)).isEqualTo(result);
     }
 
+    @Test
+    void testReduceMoneyDifferentCurrency() {
+        final Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        final Money result = bank.reduce(Money.franc(2), "USD");
+        assertThat(Money.dollar(1)).isEqualTo(result);
+    }
+
+    @Test
+    void testArrayEquals() {
+        assertThat(new Object[]{"abc"}).isEqualTo(new Object[]{"abc"});
+    }
+
+    @Test
+    void testIdentityRate() {
+        assertThat(new Bank().rate("USD", "USD")).isEqualTo(1);
+    }
+
     private interface Expression {
-        Money reduce(String to);
+        Money reduce(final Bank bank, String to);
     }
 
     private static final class Money implements Expression {
@@ -104,8 +125,9 @@ class DollarTest {
         }
 
         @Override
-        public Money reduce(final String to) {
-            return this;
+        public Money reduce(final Bank bank, final String to) {
+            final int rate = bank.rate(currency, to);
+            return new Money(amount / rate, to);
         }
     }
 
@@ -118,15 +140,47 @@ class DollarTest {
             this.addend = addend;
         }
 
-        public Money reduce(final String to) {
+        public Money reduce(final Bank bank, final String to) {
             final int amount = augend.amount + addend.amount;
             return new Money(amount, to);
         }
     }
 
     private class Bank {
+
+        private final Map<Pair, Integer> rates = new HashMap<>();
+
         public Money reduce(final Expression source, final String to) {
-            return source.reduce(to);
+            return source.reduce(this, to);
+        }
+
+        public void addRate(final String from, final String to, final int rate) {
+            rates.put(new Pair(from, to), rate);
+        }
+
+        private int rate(final String from, final String to) {
+            if (from.equals(to)) {return 1;}
+            final Integer rate = rates.get(new Pair(from, to));
+            return rate.intValue();
+        }
+    }
+
+    private class Pair {
+        private final String from;
+        private final String to;
+
+        private Pair(final String from, final String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        public boolean equals(final Object obj) {
+            final Pair pair = (Pair) obj;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
+        public int hashCode() {
+            return 0;
         }
     }
 }
