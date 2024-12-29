8b3f6bd77a95ee5d26439afb389035cb34d9c696
JinuCheon <woojin8787@gmail.com>
2024-12-30 00:01:14 +0900

    fix test to fail: 삼각측량

--- demo/src/test/java/com/example/demo/dollartest241229/DollarTest.java
+++ demo/src/test/java/com/example/demo/dollartest241229/DollarTest.java
@@ -29,5 +29,6 @@ class DollarTest {
     @Test
     void testEquality() {
         assertThat(new Dollar(5).equals(new Dollar(5))).isTrue();
+        assertThat(new Dollar(5).equals(new Dollar(6))).isFalse();
     }
 }
