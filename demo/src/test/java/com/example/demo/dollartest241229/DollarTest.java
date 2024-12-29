376eb8ef54978e73f56917feb97a5c7167f6d83b
JinuCheon <woojin8787@gmail.com>
2024-12-29 16:55:01 +0900

    할 일 목록 추가

--- demo/src/test/java/com/example/demo/dollartest241229/DollarTest.java
+++ demo/src/test/java/com/example/demo/dollartest241229/DollarTest.java
@@ -1,7 +1,13 @@
 package com.example.demo.dollartest241229;
 
 /**
- * 사내스터디 겸 2회독 시작
+ * 할 일 목록
+ *
+ * - [ ] $5+ 10CHF-$10(환율이 2:1일 경우)
+ * - [ ] $5 × 2=$10
+ * - [ ] amount를 private으로 만들기
+ * - [ ] Dollar 부작용(side effect)?
+ * - [ ] Money 반올림?
  */
 class DollarTest {
 
