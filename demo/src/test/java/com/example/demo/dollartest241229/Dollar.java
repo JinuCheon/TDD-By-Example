d54695fa9ceb55992c492fe3cbfc4ab4e61f0e50
JinuCheon <woojin8787@gmail.com>
2024-12-29 23:29:26 +0900

    fast green

--- demo/src/test/java/com/example/demo/dollartest241229/Dollar.java
+++ demo/src/test/java/com/example/demo/dollartest241229/Dollar.java
@@ -1,13 +1,11 @@
 package com.example.demo.dollartest241229;
 
 public class Dollar {
-    public int amount;
+    public int amount = 10;
 
     public Dollar(final int amount) {
-        throw new UnsupportedOperationException("Unsupported Dollar");
     }
 
     public void times(final int multiplier) {
-        throw new UnsupportedOperationException("Unsupported times");
     }
 }
