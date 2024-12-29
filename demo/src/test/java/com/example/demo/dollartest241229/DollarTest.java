d2b48539ce8b3536ba2d8fe085c09a410c4a2c09
JinuCheon <woojin8787@gmail.com>
2024-12-29 16:53:12 +0900

    1장 시작: add test

--- demo/src/test/java/com/example/demo/dollartest241229/DollarTest.java
+++ demo/src/test/java/com/example/demo/dollartest241229/DollarTest.java
@@ -4,4 +4,10 @@ package com.example.demo.dollartest241229;
  * 사내스터디 겸 2회독 시작
  */
 class DollarTest {
+
+    public void testMultiplication() {
+        final Dollar five = new Dollar(5);
+        five.times(2);
+        assertThat(five.amount).isEqualTo(10);
+    }
 }
