4f0b75aa36de2e18a32bdfb17a9186999ee7684a
JinuCheon <woojin8787@gmail.com>
2024-12-29 23:33:10 +0900

    머릿속으로 5 * 2 를 했던 것 풀어서 쓴다 -> 켄트는 5와 2가 중복이라고 한다

--- demo/src/test/java/com/example/demo/dollartest241229/Dollar.java
+++ demo/src/test/java/com/example/demo/dollartest241229/Dollar.java
@@ -1,11 +1,12 @@
 package com.example.demo.dollartest241229;
 
 public class Dollar {
-    public int amount = 10;
+    public int amount;
 
     public Dollar(final int amount) {
     }
 
     public void times(final int multiplier) {
+        amount = 5 * 2;
     }
 }
